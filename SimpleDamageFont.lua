defe90681d09700153de91b7621b530a2d620cd9
dumbrellas <430513+dumbrellas@users.noreply.github.com>
2026-07-05 12:45:39 -0400

    Working test using C_UTFileAsset function to validate db stored font path

--- SimpleDamageFont.lua
+++ SimpleDamageFont.lua
@@ -3,6 +3,7 @@ local LSM = LibStub("LibSharedMedia-3.0")
 
 
 ns.DEFAULT_FONT = DAMAGE_TEXT_FONT
+ns.DEBUG_FONT = "Interface\\AddOns\\SimpleDamageFont\\Fonts\\Expressway.ttf"
 
 
 local frame = CreateFrame("Frame")
@@ -11,30 +12,24 @@ frame:RegisterEvent("PLAYER_LOGIN")
 frame:SetScript("OnEvent", function(self, event, loadedAddon)
     if event == "ADDON_LOADED" then
         if loadedAddon == addonName then
---[[
+
             SimpleDamageFontDB = SimpleDamageFontDB or {}
             ns.db = SimpleDamageFontDB
-
-            if not ns.db.customFontName then
-                ns.db.customFontName = "Bazooka"
-            end
-
-            ns.db.customFontPath = LSM:Fetch("font", ns.db.customFontName, true) or ns.DEFAULT_FONT
-            DAMAGE_TEXT_FONT = ns.db.customFontPath
-  ]]
-            DAMAGE_TEXT_FONT  = "Path\\to\\fake\\file.ttf"          
-            self:UnregisterEvent("ADDON_LOADED")
-        end
+  
+        if C_UIFileAsset.IsLooseFile(ns.DEBUG_FONT) then
+            print "|cFF00FF00SimpleDamageFont:|r DEBUG_FONT is a loose file and can be used." -- Debug message, remove later
+            print ("|cFF00FF00SimpleDamageFont:|r Font set to: " .. ns.DEBUG_FONT) -- Debug message, remove later
+            DAMAGE_TEXT_FONT = ns.DEBUG_FONT
+        else
+            print "|cFF00FF00SimpleDamageFont:|r DEBUG_FONT is not a loose file and cannot be used." -- Debug message, remove later
+            print ("|cFF00FF00SimpleDamageFont:|r Font set to: " .. ns.DEFAULT_FONT) -- Debug message, remove later
+            DAMAGE_TEXT_FONT = ns.DEFAULT_FONT
+        end      
+        self:UnregisterEvent("ADDON_LOADED")
+    end
 
     elseif event == "PLAYER_LOGIN" then
-        --[[
-        local fontPath = LSM:Fetch("font", ns.db.customFont, true) or ns.DEFAULT_FONT
-        DAMAGE_TEXT_FONT = fontPath
 
-        -- debug (remove later)
-        print(("|cFF00FF00SimpleDamageFont:|r name=%s  path=%s")
-        :format(tostring(ns.db.customFont), tostring(fontPath)))
-        ]]
         self:UnregisterEvent("PLAYER_LOGIN")
     end
 end)

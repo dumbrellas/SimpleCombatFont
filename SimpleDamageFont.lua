5e2d96fb68994aec39fd31bd50607d1eca224e23
dumbrellas <430513+dumbrellas@users.noreply.github.com>
2026-07-05 15:44:36 -0400

    Update addon_loaded event logic
    Added nil check for first-run scenario and sets to default wow font
    Uses WoWs UIFileAsset.IsLooseFile function to verify if cached font path is still valid, else sets to default
    
    Will revisit with possible db healing if registered font is now missing but replaceable with another and provide message to user

--- SimpleDamageFont.lua
+++ SimpleDamageFont.lua
@@ -12,24 +12,24 @@ frame:RegisterEvent("PLAYER_LOGIN")
 frame:SetScript("OnEvent", function(self, event, loadedAddon)
     if event == "ADDON_LOADED" then
         if loadedAddon == addonName then
-
             SimpleDamageFontDB = SimpleDamageFontDB or {}
             ns.db = SimpleDamageFontDB
-  
-        if C_UIFileAsset.IsLooseFile(ns.DEBUG_FONT) then
-            print "|cFF00FF00SimpleDamageFont:|r DEBUG_FONT is a loose file and can be used." -- Debug message, remove later
-            print ("|cFF00FF00SimpleDamageFont:|r Font set to: " .. ns.DEBUG_FONT) -- Debug message, remove later
-            DAMAGE_TEXT_FONT = ns.DEBUG_FONT
+        if ns.db.customFontPath == nil then
+            print "|cFF00FF00SimpleDamageFont:|r Database was nil, set to default font." -- Debug message, remove later
+            DAMAGE_TEXT_FONT = ns.DEFAULT_FONT
+        elseif C_UIFileAsset.IsLooseFile(ns.db.customFontPath) then
+            print("|cFF00FF00SimpleDamageFont:|r |cFFFFD100" .. ns.db.customFontName .. "|r is a loose file and can be used.") -- Debug message, remove later
+            print("|cFF00FF00SimpleDamageFont:|r Font set to: " .. ns.db.customFontPath) -- Debug message, remove later
+            DAMAGE_TEXT_FONT = ns.db.customFontPath
         else
-            print "|cFF00FF00SimpleDamageFont:|r DEBUG_FONT is not a loose file and cannot be used." -- Debug message, remove later
-            print ("|cFF00FF00SimpleDamageFont:|r Font set to: " .. ns.DEFAULT_FONT) -- Debug message, remove later
+            print("|cFF00FF00SimpleDamageFont:|r |cFFFFD100" .. ns.db.customFontName .. "|r is not a loose file and cannot be used.") -- Debug message, remove later
+            print("|cFF00FF00SimpleDamageFont:|r Font set to: " .. ns.DEFAULT_FONT) -- Debug message, remove later
             DAMAGE_TEXT_FONT = ns.DEFAULT_FONT
         end      
         self:UnregisterEvent("ADDON_LOADED")
     end
 
     elseif event == "PLAYER_LOGIN" then
-
         self:UnregisterEvent("PLAYER_LOGIN")
     end
 end)

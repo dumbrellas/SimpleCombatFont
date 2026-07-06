2f926adbd0e51c10940b63128072741d18908703
dumbrellas <430513+dumbrellas@users.noreply.github.com>
2026-07-05 21:37:43 -0400

    Update name to new addon name, Simple Combat Font

--- SimpleDamageFont.lua
+++ SimpleDamageFont.lua
@@ -9,14 +9,14 @@ frame:RegisterEvent("PLAYER_LOGIN")
 frame:SetScript("OnEvent", function(self, event, loadedAddon)
     if event == "ADDON_LOADED" then
         if loadedAddon == addonName then
-            SimpleDamageFontDB = SimpleDamageFontDB or {}
-            ns.db = SimpleDamageFontDB
+            SimpleCombatFontDB = SimpleCombatFontDB or {}
+            ns.db = SimpleCombatFontDB
         if not ns.db.customFontPath then
             DAMAGE_TEXT_FONT = ns.DEFAULT_FONT
         elseif C_UIFileAsset.IsLooseFile(ns.db.customFontPath) or C_UIFileAsset.IsKnownFile(ns.db.customFontPath) then
             DAMAGE_TEXT_FONT = ns.db.customFontPath
         else
-            print("|cFF00FF00SimpleDamageFont:|r |cFFFFD100" .. ns.db.customFontName .. "|r was not found. Please select a different font in SDF settings.")
+            print("|cFF00FF00SimpleCombatFont:|r |cFFFFD100" .. ns.db.customFontName .. "|r was not found. Please select a different font in SCF settings.")
             DAMAGE_TEXT_FONT = ns.DEFAULT_FONT
         end      
         self:UnregisterEvent("ADDON_LOADED")
